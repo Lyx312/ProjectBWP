34282292b278cbac046ee2862236ee8cc5543adb
No <xward6464@gmail.com>
2024-01-12 16:48:13 +0700

    perubahan dikit

--- resources/views/Report.blade.php
+++ resources/views/Report.blade.php
@@ -1,41 +1,39 @@
 @extends("layout.Home")
 
 @section("content")
-
     <div class="card-columns mx-3 mb-3">
         @foreach ($order as $orders)
-        <div class="card">
-            <div class="card-header bg-primary">
-                <h5 class="card-title">Order ID: {{$orders->order_id}}</h5>
-            </div>
-            <div class="card-body">
-                <p class="card-text">Buyer: {{$orders->User->display_name}}</p>
-                <p class="card-text">Total Price: {{ number_format($orders->order_total, 0, ",", ".") }}</p>
-                <p class="card-text">Items:</p>
+            <div class="card">
+                <div class="card-header bg-primary">
+                    <h5 class="card-title">Order ID: {{$orders->order_id}}</h5>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Buyer: {{$orders->User->display_name}}</p>
+                    <p class="card-text">Total Price: {{ number_format($orders->order_total, 0, ",", ".") }}</p>
+                    <p class="card-text">Items:</p>
 
-                    @foreach ($orders->OrderDetail as $index => $detail)
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">No</th>
-                                    <th scope="col">Item ID</th>
-                                    <th scope="col">Quantity</th>
-                                    <th scope="col">Subtotal</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Item ID</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Subtotal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($orders->OrderDetail as $index => $detail)
                                 <tr>
                                     <th scope="row">{{$index+1}}</th>
                                     <td class="">{{$detail->detail_item_id}}</td>
-                                    <td> {{$detail->detail_item_quantity}}</td>
-                                    <td>  {{ number_format($detail->detail_subtotal, 0, ",", ".") }}</td>
+                                    <td>{{$detail->detail_item_quantity}}</td>
+                                    <td>{{ number_format($detail->detail_subtotal, 0, ",", ".") }}</td>
                                 </tr>
-                            </tbody>
-                        </table>
-                    @endforeach
-
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
         @endforeach
     </div>
 @endsection
